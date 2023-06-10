605e9abc6ad985915067fea8e644ad4c5562c53b
M Sanjay <msnjy12o52o@gmail.com>
2023-06-10 06:53:14 +0530

    made input's case-insensitive, toLowercase()

--- main.js
+++ main.js
@@ -12,23 +12,23 @@ function getComputerChoice() {
 }
 
 function game(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === 'Rock') {
+    if (playerSelection.toLowerCase() === "Rock" && computerSelection.toLowerCase() === 'Rock') {
         return "Rock vs. Rock: It's a tie.";
-    }   else if (playerSelection === "Rock" && computerSelection === 'Paper') {
+    }   else if (playerSelection.toLowerCase() === "Rock" && computerSelection.toLowerCase() === 'Paper') {
         return "Rock vs. Paper: Paper wins.";
-    }   else if (playerSelection === "Rock" && computerSelection === 'Scissors') {
+    }   else if (playerSelection.toLowerCase() === "Rock" && computerSelection.toLowerCase() === 'Scissors') {
         return "Rock vs. Scissors: Rock wins.";
-    }   else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    }   else if (playerSelection.toLowerCase() === "Paper" && computerSelection.toLowerCase() === "Rock") {
         return "Paper vs. Rock: Rock wins.";
-    }   else if (playerSelection === "Paper" && computerSelection === 'Paper') {
+    }   else if (playerSelection.toLowerCase() === "Paper" && computerSelection.toLowerCase() === 'Paper') {
         return "Paper vs. Paper: It's a tie.";
-    }   else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    }   else if (playerSelection.toLowerCase() === "Paper" && computerSelection.toLowerCase() === "Scissors") {
         return "Papers vs. Scissors: Scissors win.";
-    }   else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    }   else if (playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Rock") {
         return "Scissor's vs. Rock: Rock wins.";
-    }   else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    }   else if (playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Paper") {
         return "Scissor's vs. Paper: Scissors win.";
-    }   else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+    }   else if (playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Scissors") {
         return "Scissors vs. Scissors: It's a tie";
     }
 }       
