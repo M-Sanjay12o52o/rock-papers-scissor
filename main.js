7a405ff3d4365a2b35324f7bb9dcecd3417a9cca
M Sanjay <msnjy12o52o@gmail.com>
2023-06-10 07:33:23 +0530

    sort of completed, scores not getting updated

--- main.js
+++ main.js
@@ -15,32 +15,62 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === 'rock') {
         return "Rock vs. Rock: It's a tie.";
     }   else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === 'paper') {
-        return "rock vs. paper: Paper wins.";
+        return "Rock vs. Paper: You lose.";
     }   else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === 'scissors') {
-        return "Rock vs. Scissors: rock wins.";
+        return "Rock vs. Scissors: You win.";
     }   else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        return "Paper vs. Rock: Rock wins.";
+        return "Paper vs. Rock: You lose.";
     }   else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === 'paper') {
         return "Paper vs. Paper: It's a tie.";
     }   else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
-        return "Papers vs. Scissors: Scissors win.";
+        return "Papers vs. Scissors: You lose.";
     }   else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
-        return "Scissor's vs. Rock: Rock wins.";
+        return "Scissor's vs. Rock: You lose.";
     }   else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-        return "Scissor's vs. paper: scissors win.";
+        return "Scissor's vs. paper: You win.";
     }   else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors") {
-        return "Scissors vs. Scissors: It's a tie";
+        return "Scissors vs. Scissors: It's a tie.";
     }
 
     return "Invalid selection. Please choose rock, paper, or scissors.";
 }    
 
-// var player = "Rock";
+
 // var computer = "Paper";
-// var result = game(player, computer);
+// var result = playRound(player, computer);
 // console.log(result);
 
 
 // console.log(getComputerChoice());
 
 // console.log(game('Rock', 'Rock'));
+
+let player = "Rock";
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+
+    for (i = 0; i < 5; i++) {
+        // console.log(playRound(player, getComputerChoice()));
+        let result = playRound(player, getComputerChoice());
+        console.log(result);
+
+        if (result === "You win.") {
+            userScore += 1;
+            // console.log(userScore);
+        } else if (result === "You lose.") {
+            computerScore += 1;
+            // console.log(computerScore); 
+        }
+    }
+
+    console.log("Final scores:");
+    console.log("User score:", userScore);
+    console.log("Computer score:", computerScore);
+
+    return [userScore, computerScore];
+}
+
+game();
+
